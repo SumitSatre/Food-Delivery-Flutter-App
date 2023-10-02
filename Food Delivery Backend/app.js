71be30e6f23d970fe056d156e8f4505b78e1c540
Sumit Satre <sumitsatre03@gmail.com>
2023-10-02 08:40:10 +0530

    Backend Change

--- Food Delivery Backend/app.js
+++ Food Delivery Backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // It is middleware whch allows the specified server to access the backend server 
 app.use((req , res , next)=>{
-    res.setHeader("Access-Control-Allow-Origin" , process.env.FrontendWebAddress);
+    res.setHeader("Access-Control-Allow-Origin" , '*');
     res.header(
         "Access-Control-Allow-Headers",
         "Origin , X-Requested-With , Content-Type , Accept"
